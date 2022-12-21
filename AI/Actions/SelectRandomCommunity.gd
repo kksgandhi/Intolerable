ee4d21e7d98d0785c5886d64431b2b6e7f323e02
Kutub Gandhi <kksgandhi@gmail.com>
2022-12-21 16:44:27 -0500

    create select random community action

---
+++ AI/Actions/SelectRandomCommunity.gd
@@ -0,0 +1,9 @@
+extends ActionLeaf
+
+onready var communities := get_tree().get_nodes_in_group("community")
+
+func tick(actor, _blackboard):
+  var random_community:Node2D = communities[randi() % communities.size()]
+  actor.POI = random_community.global_position
+  actor.has_entered_poi = false
+  return SUCCESS
